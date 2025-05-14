46eaa8b1c966730524ad8564c8e95c43edb733d6
Deivid-Dev1 <davidmrquez3@gmail.com>
2025-05-14 14:16:46 +0200

    fix1

--- app/api/items/[itemId]/route.ts
+++ app/api/items/[itemId]/route.ts
@@ -1,29 +1,30 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function PATCH(req: Request, {params}: {params: {itemId: string} }
-
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ itemId: string }> }
 ) {
-    try {
-        const {itemId} =  await params;
-        const values = await req.json();
+  try {
+    const { itemId } = await params;
+    const values = await req.json();
 
-        if (!itemId) {
-            return new NextResponse("Unauthorized", {status: 401})
-        }
-
-        const element = await db.element.update({
-            where: {
-                id: itemId,
-            },
-            data: {
-                ...values,
-            }
-        })
-
-        return NextResponse.json(element)
-    }catch (error) {
-        console.log(error);
-        return new NextResponse("Internal error", {status: 500})
+    if (!itemId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const element = await db.element.update({
+      where: {
+        id: itemId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    return NextResponse.json(element);
+  } catch (error) {
+    console.error("Error updating element:", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
 }
